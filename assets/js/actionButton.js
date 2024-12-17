3277185dfc8de7aae79aec91d09a819045316768
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-12-17 19:34:21 -0300

    Add files via upload

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -1,1 +1,24 @@
-document.addEventListener("DOMContentLoaded",function(){"#inicio"!==window.location.hash&&(window.location.hash="#inicio");let t=document.getElementById("backToTop"),e=document.getElementById("whatsappButton"),n=document.querySelectorAll(".cta"),o=document.querySelectorAll(".cta-servicos"),c="https://wa.me/5561993430859";n.forEach(function(t){t.addEventListener("click",function(){window.open(c)})}),o.forEach(function(t){t.addEventListener("click",function(){window.open(c)})}),t.addEventListener("click",function(){window.scrollTo({top:0,behavior:"smooth"})}),e.addEventListener("click",function(){window.open(c)})});
+document.addEventListener("DOMContentLoaded", function () {
+  "#inicio" !== window.location.hash && (window.location.hash = "#inicio");
+  let t = document.getElementById("backToTop"),
+    e = document.getElementById("whatsappButton"),
+    n = document.querySelectorAll(".cta"),
+    o = document.querySelectorAll(".cta-servicos"),
+    c = "https://wa.me/5561993430859";
+  n.forEach(function (t) {
+    t.addEventListener("click", function () {
+      window.open(c);
+    });
+  }),
+    o.forEach(function (t) {
+      t.addEventListener("click", function () {
+        window.open(c);
+      });
+    }),
+    t.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }),
+    e.addEventListener("click", function () {
+      window.open(c);
+    });
+});
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,26 @@
-document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("menuToggle"),t=document.querySelector(".menu-aberto"),s=document.querySelectorAll(".nav-menu a"),c=document.body;e.addEventListener("change",function(){e.checked?(t.classList.add("active"),c.classList.add("no-scroll")):(t.classList.remove("active"),c.classList.remove("no-scroll"))}),s.forEach(s=>{s.addEventListener("click",function(){e.checked=!1,t.classList.remove("active"),c.classList.remove("no-scroll")})}),document.querySelector("#link-inicio").classList.add("active");let a=document.querySelectorAll("#navigation-menu-list a");a.forEach(e=>{e.addEventListener("click",()=>{a.forEach(e=>e.classList.remove("active")),e.classList.add("active")})})});const buttonResize=()=>{let e=document.querySelectorAll(".cta");e.forEach(()=>{let e=document.querySelector(".cta");window.innerWidth<1024&&(e.classList.remove("cta"),e.classList.add("cta-servicos"))})};window.addEventListener("resize",buttonResize),window.addEventListener("load",buttonResize);
+document.addEventListener("DOMContentLoaded", function () {
+  let e = document.getElementById("menuToggle"),
+    t = document.querySelector(".menu-aberto"),
+    s = document.querySelectorAll(".nav-menu a"),
+    c = document.body;
+  e.addEventListener("change", function () {
+    e.checked
+      ? (t.classList.add("active"), c.classList.add("no-scroll"))
+      : (t.classList.remove("active"), c.classList.remove("no-scroll"));
+  }),
+    s.forEach((s) => {
+      s.addEventListener("click", function () {
+        (e.checked = !1),
+          t.classList.remove("active"),
+          c.classList.remove("no-scroll");
+      });
+    }),
+    document.querySelector("#link-inicio").classList.add("active");
+  let a = document.querySelectorAll("#navigation-menu-list a");
+  a.forEach((e) => {
+    e.addEventListener("click", () => {
+      a.forEach((e) => e.classList.remove("active")), e.classList.add("active");
+    });
+  });
+});
+
