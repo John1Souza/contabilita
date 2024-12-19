5c98119dd882b5db046658e82164651ea8059303
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-12-19 18:48:17 -0300

    Add files via upload

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   "#inicio" !== window.location.hash && (window.location.hash = "#inicio");
-  let t = document.getElementById("backToTop"),
+  const t = document.getElementById("backToTop"),
     e = document.getElementById("whatsappButton"),
     n = document.querySelectorAll(".cta"),
     o = document.querySelectorAll(".cta-servicos"),
@@ -21,4 +21,21 @@ document.addEventListener("DOMContentLoaded", function () {
     e.addEventListener("click", function () {
       window.open(c);
     });
+    
 });
+
+
+const menuLinks = document.querySelectorAll("#navigation-menu-list a");
+
+function removeActiveClass() {
+    menuLinks.forEach(link => {
+        link.classList.remove("active");
+    });
+}
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        removeActiveClass(); 
+        link.classList.add("active");
+    });
+});
+document.querySelector("#link-inicio").classList.add("active");
