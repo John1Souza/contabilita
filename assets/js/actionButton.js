e067a11b987214570a88ba4f48b5ed3c0f1c67dc
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2025-01-14 16:42:01 -0300

    Add files via upload

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -39,3 +39,19 @@ menuLinks.forEach(link => {
     });
 });
 document.querySelector("#link-inicio").classList.add("active");
+
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
